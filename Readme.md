d95982f93295664a16ff94d15e7a4d0174564fcf
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-04-17 18:25:49 +0530

    different deploymet environments

---
+++ Readme.md
@@ -0,0 +1,18 @@
+### To Run 
+
+<pre>
+
+1) npm i
+2) serve -s build (for local sever - it run build file)
+
+----------
+for run different environments (visit package.json file):
+
+3) npm run build:dev <-- (development build)
+3) npm run build:staging <--(staging build)
+3) npm run build:qa <--(QA build)
+3) npm run build:pod <-- (production build)
+
+</pre>
+
+reference : https://create-react-app.dev/docs/deployment/
---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import UserList from './components/UserList/UserList';
+import UserProfile from './components/UserProfile/UserProfile';
+import './App.css';
+
+function App() {
+  const userProfileData = useSelector((state) => state.userProfile);
+  
+  console.log("Build Type",process.env.REACT_APP_ENV)
+  return (
+    <>
+      <div className='App container-fluid'>
+        <div className='user-list-main-container'>
+          <div className={userProfileData.length === 0 ? 'user-list' : 'user-list user-list-sm'}>
+            <UserList />
+          </div>
+          <div className='user-profile'>
+            {
+              userProfileData.length !==0 ? <UserProfile userProfileData={userProfileData}/> : ''
+            }
+          </div>
+        </div>
+      </div> 
+    </>
+  );
+}
+
+export default App;
